11a36814f72e03d895ffb3bde547a8396ec4e7d3
grapewhyne <postvikash@outlook.com>
2024-01-03 17:13:04 -0500

    Jan 3, 2024 - Problem of the day - Roman to Integer

---
+++ src/main/java/org/example/RomanToInteger.java
@@ -0,0 +1,107 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ *
+ *     I can be placed before V (5) and X (10) to make 4 and 9.
+ *     X can be placed before L (50) and C (100) to make 40 and 90.
+ *     C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ * Given a roman numeral, convert it to an integer.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ *
+ * Example 2:
+ *
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ *
+ * Example 3:
+ *
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *     1 <= s.length <= 15
+ *     s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+ *     It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+ */
+
+public class RomanToInteger {
+
+    public static void main(String [] args) {
+        String s = "MCMXCIV";
+        int answer = romanToInt(s);
+        System.out.println(answer);
+    }
+    public static int romanToInt(String s) {
+        Map<Character, Integer> characterValueMap = new HashMap<>() {{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+        if (s.length() == 1)
+            return characterValueMap.get(s.charAt(0));
+        if (s.length() == 2 && characterValueMap.get(s.charAt(1)) > characterValueMap.get(s.charAt(0)))
+            return characterValueMap.get(s.charAt(1)) - characterValueMap.get(s.charAt(0));
+        if (s.length() == 2)
+            return characterValueMap.get(s.charAt(1)) + characterValueMap.get(s.charAt(0));
+        int answer = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i==0 && characterValueMap.get(s.charAt(1)) > characterValueMap.get(s.charAt(0))) {
+                answer = characterValueMap.get(s.charAt(1)) - characterValueMap.get(s.charAt(0));
+                i++;
+            } else if (i==0) {
+                answer = characterValueMap.get(s.charAt(0));
+            } else if (i+2 <= s.length() && characterValueMap.get(s.charAt(i+1)) <= characterValueMap.get(s.charAt(i))) {
+                answer = answer + characterValueMap.get(s.charAt(i));
+                if (i+2 == s.length()) {
+                    answer = answer + characterValueMap.get(s.charAt(i + 1));
+                    break;
+                }
+            } else if (i+2 <= s.length() && characterValueMap.get(s.charAt(i+1)) > characterValueMap.get(s.charAt(i))) {
+                answer = answer + (characterValueMap.get(s.charAt(i+1)) - characterValueMap.get(s.charAt(i)));
+                if (i+2 == s.length()) {
+                    break;
+                }
+                i++;
+            } else if (i == s.length()-1 && characterValueMap.get(s.charAt(i-1)) >= characterValueMap.get(s.charAt(i))) {
+                answer = answer + characterValueMap.get(s.charAt(i));
+            } else if (i == s.length() - 1 && characterValueMap.get(s.charAt(i-1)) < characterValueMap.get(s.charAt(i))) {
+                answer = answer - characterValueMap.get(s.charAt(i));
+            }
+        }
+        return answer;
+    }
+}
