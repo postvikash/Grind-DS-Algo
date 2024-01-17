9f8fbba35f302112995c43eac97ce5276c62c42e
grapewhyne <postvikash@outlook.com>
2024-01-17 07:35:37 -0500

    Build issue was due to missing String [] args in main function. Also committing question of the day WordPattern.

--- src/main/java/org/example/WordPattern.java
+++ src/main/java/org/example/WordPattern.java
@@ -1,16 +1,30 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordPattern {
     public static boolean wordPattern(String pattern, String s) {
-        String [] words = s.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            System.out.println(words[i]);
+        String [] words = s.split("\\s+");
+        if (pattern.length() != words.length)
+            return false;
+        Map<Character, String> characterStringMap = new HashMap<>(26);
+        Map<String, Character> stringCharacterHashMap = new HashMap<>(26);
+        for (int i = 0; i < words.length && i < pattern.length(); i++) {
+            if (characterStringMap.get(pattern.charAt(i)) == null)
+                characterStringMap.put(pattern.charAt(i), words[i]);
+            if (stringCharacterHashMap.get(words[i]) == null) {
+                stringCharacterHashMap.put(words[i], pattern.charAt(i));
+            }
+            if (!characterStringMap.get(pattern.charAt(i)).equals(words[i]))
+                return false;
+            if(stringCharacterHashMap.get(words[i]) != pattern.charAt(i))
+                return false;
         }
-        System.out.println(pattern);
         return true;
     }
 
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println(wordPattern("abba", "cat dog dog cat"));
     }
 }
