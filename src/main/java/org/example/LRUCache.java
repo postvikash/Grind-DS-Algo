73bebfcfec6dee34b16f9151537cf9f7406ad374
Vikash Kumar <postvikash@outlook.com>
2024-02-16 16:37:11 -0500

    Optimized LRU Cache

--- src/main/java/org/example/LRUCache.java
+++ src/main/java/org/example/LRUCache.java
@@ -1,27 +1,39 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LRUCache {
     int capacity;
     int size = 0;
     PageNode head = null, tail = null;
+    Map<Integer, PageNode> keyNodeAddressMap = new HashMap<>();
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        PageNode itr = head;
-        while(itr != null) {
-            if( itr.key == key) {
-                delinkPageAndPutOnTop(itr);
-                return itr.val;
-            }
-            itr = itr.next;
-        }
-        return -1;
+        PageNode detachedNode = keyNodeAddressMap.get(key);
+        if(detachedNode == null)
+            return -1;
+        else
+            delinkPageAndPutOnTop(detachedNode);
+        return detachedNode.val;
     }
 
     public void put(int key, int value) {
+        if (get(key) != -1) {
+            PageNode itr = head;
+            while(itr != null) {
+                if( itr.key == key) {
+                    itr.val = value;
+                    delinkPageAndPutOnTop(itr);
+                    return;
+                }
+                itr = itr.next;
+            }
+        }
         if (size < capacity) {
             size++;
             insertNewPageInCache(key, value);
@@ -39,9 +51,16 @@ public class LRUCache {
             head.prev = pageNode;
             head = pageNode;
         }
+        keyNodeAddressMap.put(key, pageNode);
     }
 
     protected void evictAndInsertPage(int key, int value) {
+        keyNodeAddressMap.remove(tail.key);
+        if (size == 1) {
+            head = tail = null;
+            insertNewPageInCache(key, value);
+            return;
+        }
         tail = tail.prev;
         tail.next = null;
         insertNewPageInCache(key, value);
@@ -63,6 +82,7 @@ public class LRUCache {
         page.next.prev = page.prev;
         page.prev = null;
         page.next = head;
+        head.prev = page;
         head = page;
     }
 
@@ -75,4 +95,33 @@ public class LRUCache {
         }
         System.out.println("\n");
     }
+
 }
+
+
+class PageNode {
+    int key;
+    int val;
+    PageNode next;
+    PageNode prev;
+    PageNode() {}
+    PageNode(int key, int val) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+    PageNode(int key, int val, PageNode next) {
+        this.key = key;
+        this.val = val;
+        this.next = next;
+        this.prev = null;
+    }
+    PageNode(int key, int val, PageNode next, PageNode prev) {
+        this.key = key;
+        this.val = val;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
